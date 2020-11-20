cd2e2f5dc6e9aec8af7e4ca6b163c23823866bd7
Aleksandr <fattybobcat@gmail.com>
2020-11-20 14:00:47 +0300

    add little bit

--- datab.py
+++ datab.py
@@ -9,14 +9,18 @@ class BDBot():
             self.cur = self.con.cursor()
             self.cur.execute("CREATE TABLE IF NOT EXISTS game ("
                         "id INTEGER PRIMARY KEY,"
-                        "chat_id INTEGER UNIQUE NOT NULL)")
+                        "chat_id INTEGER UNIQUE NOT NULL,"
+                        "admin_id INTEGER)")
             self.con.commit()
 
     def new_game(self, chat_id):
         entities = (chat_id,)
         sql = "INSERT INTO game (chat_id) VALUES(?)"
         print(sql, chat_id)
-        self.cur.execute(sql, entities)
+        try:
+            self.cur.execute(sql, entities)
+        except sqlite3.IntegrityError:
+            print('игра есть, нужно удалить ее таблицу и начать заново')
         self.con.commit()
 
     def delete_game(self, chat_id):
@@ -33,6 +37,9 @@ class BDBot():
         return all_results
 
 
+#class DBGame(BDBot):
+#https://pyneng.github.io/pyneng-3/db-foreign-key/   создавать внешние ключи.
+#https://proglib.io/p/kak-podruzhit-python-i-bazy-dannyh-sql-podrobnoe-rukovodstvo-2020-02-27     - отличное пособие для изучения
 
 vv = BDBot()
 vv.new_game(212)
